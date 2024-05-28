660253942394a6e3c6d66fc4bd2664fc9c4f03d0
Nilanjan Chowdhury <78751630+Nil69420@users.noreply.github.com>
2024-05-28 19:00:37 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,24 @@
 #include <std_srvs/Trigger.h>
 #include <stm_client/relay_control.h>
 #include <thread>
-
+#include <chrono>
 
 ros::ServiceClient pump_off_client;
+ros::ServiceClient relay_toggle_client;
 
 // void pumpOffCallback(const ros::TimerEvent&)
 // {
 //     std_srvs::Trigger srv;
 
-    
 //     if (pump_off_client.call(srv))
 //     {
 //         if (srv.response.success)
 //         {
-//             ROS_INFO("Successfully called stop pump service: %s", srv.response.message.c_str());
+//             ROS_INFO("Pump turned off successfully");
 //         }
 //         else
 //         {
-//             ROS_WARN("Failed to call stop motors service: %s", srv.response.message.c_str());
+//             ROS_WARN("Failed to turn off the pump");
 //         }
 //     }
 //     else
@@ -28,14 +28,30 @@ ros::ServiceClient pump_off_client;
 //     }
 // }
 
-bool relayToggleCallback(stm_client::relay_control::Request &req, stm_client::relay_control::Response &res)
+// void relayToggleCallback(const stm_client::relay_control::Request &req, stm_client::relay_control::Response &res)
+// {   
+    
+//}
+
+int main(int argc, char **argv)
 {
-    if (req.data == 4)
+    ros::init(argc, argv, "relay_monitor_node");
+    ros::NodeHandle nh;
+
+    pump_off_client = nh.serviceClient<std_srvs::Trigger>("/pump_off_service");
+
+    //ros::ServiceServer service = nh.advertiseService("/relay_toggle_channel", relayToggleCallback);
+
+    relay_toggle_client = nh.serviceClient<stm_client::relay_control>("/relay_toggle_channel");
+    ROS_INFO("Relay monitor node is ready");
+
+    stm_client::relay_control srv2;
+    if (srv2.request.data == 4)
     {
         ROS_INFO("Received data: 4. Will trigger /pump_off_service in 1 second");
 
         
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
         
         std_srvs::Trigger srv;
@@ -57,30 +73,11 @@ bool relayToggleCallback(stm_client::relay_control::Request &req, stm_client::re
     }
     else
     {
-        ROS_INFO("Received data: %d. No action taken", req.data);
+        ROS_INFO("Received data: %d. No action taken");
     }
 
-    res.response = true;
-    return true;
-}
+    //res.response = true;
 
-int main(int argc, char **argv)
-{
-   
-    ros::init(argc, argv, "relay_monitor_node");
-
-   
-    ros::NodeHandle nh;
-
-    
-    pump_off_client = nh.serviceClient<std_srvs::Trigger>("/pump_off_service");
-
-    
-    ros::ServiceServer service = nh.advertiseService("relay_toggle_channel", relayToggleCallback);
-    
-    ROS_INFO("Relay monitor node is ready");
-
-    
     ros::spin();
 
     return 0;
